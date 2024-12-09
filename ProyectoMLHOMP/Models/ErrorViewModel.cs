196037dc5914f416d474f9b451c00069ac7830c3
Hans Ortiz <127800719+HansOr04@users.noreply.github.com>
2024-12-08 23:01:09 -0500

    Delete ProyectoMLHOMP directory

--- ProyectoMLHOMP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProyectoMLHOMP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
